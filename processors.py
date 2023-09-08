bac73a9521fdc76e1f926af99f5349d3c1f8285d
zamp <xythec@gmail.com>
2023-09-08 17:25:19 +0300

    Added frame skipping

--- processors.py
+++ processors.py
@@ -8,6 +8,7 @@ import subprocess
 import threading
 import ebsynth_window
 from configparser import SectionProxy
+from glob import glob
 
 def run_stable_diffusion(input_file_path:str, src_file_path:str, output_file_path:str, config:SectionProxy):
 	image = comfyui.process_image(input_file_path, src_file_path, config)
@@ -27,8 +28,18 @@ def process_comfyui(config:SectionProxy):
 	if not os.path.exists(output_dir):
 		os.makedirs(output_dir)
 
+	limit_frames = config.getint("SkipFrames", fallback=0) + 1
 	files = util.get_png_files(input_dir)
+	start_frame = int(files[0][:-4])
 	for file in files:
+		frame = int(file[:-4])
+
+		# skip frames that are outside of limit
+		if (frame - start_frame) % limit_frames != 0:
+			# straight copy input to output if we're not running comfyui on this frame
+			shutil.copy(input_dir+file, output_dir+file)
+			continue
+
 		run_stable_diffusion(input_dir+file, video_dir+file, output_dir+file, config)
 
 def clamp(num, min_value, max_value):
@@ -37,23 +48,6 @@ def clamp(num, min_value, max_value):
 def divide_chunks(l, n):
 	for i in range(0, len(l), n):
 		yield l[i:i + n]
-
-def blend_img(ebsynth_dir, input_dir, frame, offset_frame, file_name_length, ext, alpha):
-	warped_img_path = f"{ebsynth_dir}{str(offset_frame).zfill(file_name_length)}/{str(frame).zfill(file_name_length)}{ext}"
-	output_img_path = f"{input_dir}{str(frame).zfill(file_name_length)}{ext}"
-
-	if not os.path.exists(warped_img_path) or not os.path.exists(output_img_path):
-		return
-
-	warped_img = Image.open(warped_img_path).convert("RGB")
-	output_img = Image.open(output_img_path).convert("RGB")
-
-	offset = abs(int(frame) - int(offset_frame))
-
-	a = alpha / offset
-
-	output_img = Image.blend(output_img, warped_img, a)
-	output_img.save(f"{input_dir}{str(frame).zfill(file_name_length)}{ext}")
 
 def wait_for_ebsynth_to_complete(automatic, process:subprocess.Popen):
 	if automatic:
@@ -75,13 +69,29 @@ def run_ebsynth(automatic, project, ebsynth_exe):
 	
 	os.remove(ebs_file)
 
+def blend_img(ebsynth_dir, input_dir, frame, offset_frame, file_name_length, ext, alpha):
+	warped_img_path = f"{ebsynth_dir}{str(offset_frame).zfill(file_name_length)}/{str(frame).zfill(file_name_length)}{ext}"
+	output_img_path = f"{input_dir}{str(frame).zfill(file_name_length)}{ext}"
+
+	if not os.path.exists(warped_img_path) or not os.path.exists(output_img_path):
+		return
+
+	warped_img = Image.open(warped_img_path).convert("RGB")
+	output_img = Image.open(output_img_path).convert("RGB")
+
+	offset = abs(int(frame) - int(offset_frame))
+
+	a = alpha / offset
+
+	output_img = Image.blend(output_img, warped_img, a)
+	output_img.save(f"{input_dir}{str(frame).zfill(file_name_length)}{ext}")
+
 def process_ebsynth(config:SectionProxy):
 	ebsynth_dir = config.get("EbsynthDir")
 	video_dir = config.get("VideoDir")
 	input_dir = config.get("InputDir")
 	output_dir = config.get("OutputDir")
 
-	alpha = config.getfloat("Alpha")
 	frame_spread = config.getint("FrameSpread")
 	ebsynth_exe = config.get("EbsynthExe")
 	max_ebsynth_files = config.getint("MaxEbsynthFiles")
@@ -107,8 +117,17 @@ def process_ebsynth(config:SectionProxy):
 	project = EBSynthProject(video_path, keys_path, "", False)
 	project.keyFrames = []
 
+	limit_frames = config.getint("SkipFrames", fallback=0) + 1
+	frame_spread = max(limit_frames, frame_spread)
+
+	start_frame = int(input_files[0][:-4])
+
 	for file in input_files:
 		frame = int(file[:-4])
+
+		# skip frames that are outside of limit
+		if (frame - start_frame) % limit_frames != 0:
+			continue
 
 		ebsynth_output_dir = f"{ebsynth_dir}{str(frame).zfill(file_name_length)}/[{filenumber_hashes}]{ext}"
 		
@@ -139,12 +158,33 @@ def process_ebsynth(config:SectionProxy):
 	run_ebsynth(automatic, project, ebsynth_exe)
 
 	# blend ebsynth files now that they are generated
+
+	files = []
+	start_dir = ebsynth_dir
+	pattern = f"*{ext}"
+
+	for dir,_,_ in os.walk(start_dir):
+		files.extend(glob(os.path.join(dir,pattern))) 
+
 	for file in input_files:
-		frame = int(file[:-4])
-		for offset in range(1, frame_spread+1):
-			if not config.getboolean("ForwardOnly", fallback=False):
-				blend_img(ebsynth_dir, input_dir, frame, frame - offset, file_name_length, ext, alpha)
-			blend_img(ebsynth_dir, input_dir, frame, frame + offset, file_name_length, ext, alpha)
+		blendable_files = []
+
+		for blend_file in files:
+			f = blend_file[-(file_name_length + 4):]
+			if f == file:
+				blendable_files.append(blend_file)
+		
+		count = len(blendable_files)
+		for blend_file in blendable_files:
+			output_img_path = f"{input_dir}{file}"
+
+			blend_img = Image.open(blend_file).convert("RGB")
+			output_img = Image.open(output_img_path).convert("RGB")
+
+			a = 1 / count
+
+			output_img = Image.blend(output_img, blend_img, a)
+			output_img.save(output_img_path)
 	
 	shutil.rmtree(ebsynth_dir)
 
