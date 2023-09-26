98a320ae12fc6c58d1d7cfcf19d98034a1df20bc
axb2035 <axb2035@gmail.com>
2023-09-27 07:12:52 +0930

    Exit if no render passes found

--- main.py
+++ main.py
@@ -49,6 +49,10 @@ def main():
 		else:
 			print(f"Could not find RenderPassFile: {render_pass_file}")
 			return
+	
+	if not any([c.startswith('RenderPass') for c in config]):
+		print('No renderpasses found in configuration files.')
+		return
 
 	if not "VideoDir" in defaults:
 		print(f"Error: Could not find VideoDir in config.")
